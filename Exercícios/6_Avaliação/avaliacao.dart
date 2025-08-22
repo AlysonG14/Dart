27954960f27430e312f6021db5a7fd067c669d6b
AlysonG14 <alyson.bosch.senai14@gmail.com>
2025-08-22 17:02:18 -0300

    Upload File 22/08

--- Exercícios/6_Avaliação/avaliacao.dart
+++ Exercícios/6_Avaliação/avaliacao.dart
@@ -2,61 +2,85 @@
 
 import "dart:io";
 
-void main(){
-
-  print("Informe o seu nome: ");
-  String nome = stdin.readLineSync()!;
-
-  while (nome == null || nome.isEmpty) {
-   print("O nome não pode ser nulos ou vazios, tente novamente: ");
-   nome = stdin.readLineSync()!;
-  } 
-
-  print("Nome: ${nome.trim()}");
-
-  print("Informe o seu CPF: ");
-  String? entrada = stdin.readLineSync()!;
-  int? CPF = int.parse(entrada);
-  
+void main() {
   try {
-
-  while (CPF == 0 || CPF == null) {
-    print("O CPF não pode ser 0, nem negativo, nem nulo e nem vazio, tente novamente: ");
-    entrada = stdin.readLineSync()!;
-  };
-
-  }catch(e){
-    print("Mensagem de Erro: Não pode converter número em string. Tente novamente: ");
-    entrada = stdin.readLineSync()!;
+    print("Informe o seu nome: ");
+    String? nome = stdin.readLineSync()!;
+    while (nome == null || nome == nome.isEmpty) {
+      print("O nome não pode ser nulo e vazio, tente novamente: ");
+      nome = stdin.readLineSync()!;
+    }
+    print("Nome: ${nome.trim()}");
+  } catch (e) {
+    print("Mensagem de Erro: A string ");
   }
 
-  do {
-    print("O CPF não pode ser negativo, tente novamente: ");
-    entrada = stdin.readLineSync()!;
-  } while(CPF <= 1);
+  try {
+    print("Informe o seu CPF: ");
+    String? entrada = stdin.readLineSync()!;
+    int? CPF = int.parse(entrada);
+    while (CPF == null || CPF.isNaN || CPF == 0) {
+      print(
+        "O CPF não pode ser 0, nem negativo, nem nulo e nem vazio, tente novamente: ",
+      );
+      entrada = stdin.readLineSync()!;
+    }
+    print("CPF: ${CPF}");
+  } catch (e) {
+    print("Erro: O int não pode ser convertido em String");
+  }
 
-  print("CPF: ${CPF}");
+  List<String> saborPizzaria = [
+    "Requeijao",
+    "Peperonni",
+    "Mussarela",
+    "Chocolate",
+  ];
+  List<double> preco = [20.00, 50.00, 46.99, 89.99];
+  dynamic quantidade = [5, 6, 8, 10];
+  List<String> carrinhoUsuario = [];
+  Map<String, dynamic> quantidadeSabor = Map.fromIterables(
+    saborPizzaria,
+    quantidade,
+  );
 
-  List <String> saborPizzaria = ["Requeijão", "Peperonni", "Mussarela", "Chocolate"];
-  List <double> precoPizzaria = [ 20.00, 50.00, 46.99, 89.99 ];
-  List <int> quantidadePizzaria = [ 5, 6, 8, 10 ];
+  int contador = 0;
 
-  List <int> carrinhoUsuario = [];
-
-  Map <String, double> sabores = Map.fromIterables(saborPizzaria, precoPizzaria);
-
-  // Map <int, Map<String, double>> s = Map.fromIterables(quantidadePizzaria, sabores);
-
-  int contador = 1;
-
-  for(String sabor in saborPizzaria){
+  print("Sabores disponíveis: ");
+  for (String? sabor in saborPizzaria) {
     print("${contador += 1}- ${sabor}");
-  };
+  }
 
-  print("Qual sabor você deseja escolher?");
-  String? usuarioEscolheSabor = stdin.readLineSync()!;
-  saborPizzaria.add(carrinhoUsuario);
+  print("Quantidade de Sabores: ");
+  print(quantidadeSabor);
 
+  while (true) {
+    print("Qual sabor você deseja? Escolhe a opção (Digite 'S' para sair) ");
+    String? usuarioEscolheSabor = stdin.readLineSync()!;
+    if (usuarioEscolheSabor == "s".toLowerCase() ||
+        usuarioEscolheSabor == "s".toUpperCase()) {
+      break;
+    }
+    if (saborPizzaria[0] == usuarioEscolheSabor ||
+        saborPizzaria[1] == usuarioEscolheSabor ||
+        saborPizzaria[2] == usuarioEscolheSabor ||
+        saborPizzaria[3] == usuarioEscolheSabor) {
+      {
+        print("Qual quantidade você deseja adicionar? ");
+        dynamic quantidadeUsuario = int.parse(stdin.readLineSync()!);
+        quantidadeUsuario -= quantidade[0];
+        print("Produto adicionado!");
+        print("Quantidade restante: ${quantidade}");
+        if (quantidade == 0) {
+          print("Estoque insuficiente, tente outro sabor!");
+          break;
+        }
+      }
+      carrinhoUsuario.add(usuarioEscolheSabor!);
+    } else {
+      print("Produto não Existe");
+    }
+  }
 
-
+  print("Carrinho: ${carrinhoUsuario}");
 }
