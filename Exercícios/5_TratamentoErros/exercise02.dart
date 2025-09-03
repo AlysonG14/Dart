38ef8dee76c869e87675cd402bf3ffcf97977183
AlysonG14 <alyson.bosch.senai14@gmail.com>
2025-09-03 11:18:12 -0300

    Upload File 03/09 - Feature: Uploading exercise 02 to finishing

--- Exercícios/5_TratamentoErros/exercise02.dart
+++ Exercícios/5_TratamentoErros/exercise02.dart
@@ -4,8 +4,7 @@ import 'dart:io';
 void main() {
 
   dynamic escolheProduto;
-  int? valorCompra;
-  double valor_desejada;
+  double valorCompra;
 
   List<Map<String, dynamic>> cantinaEscolar = [
     {"nome": 'Hamburguer de Siri', "preco": null},
@@ -19,46 +18,92 @@ void main() {
 
   print("Escolhe o produto que você deseja:");
   escolheProduto = stdin.readLineSync();
-
+  
+  do {
+    switch (escolheProduto) {
+      case 'Hamburguer de Siri':
   try {
     print("Qual valor você deseja informar?");
-    valorCompra = int.parse(stdin.readLineSync()!);
-    while (valorCompra!.isNaN || valorCompra == 0 || valorCompra.isNegative) {
+    valorCompra = double.parse(stdin.readLineSync()!);
+    while (valorCompra.isNaN || valorCompra == 0 || valorCompra.isNegative) {
       print("O valor da compra não pode ser nulo, negativo e igual a 0");
-      valorCompra = int.parse(stdin.readLineSync()!);
+      valorCompra = double.parse(stdin.readLineSync()!);
     }
     FormatException(e) {
       print("Erro: Não pode ser convertido em String. Digite mais uma vez!");
-      valorCompra = int.parse(stdin.readLineSync()!);
+      valorCompra = double.parse(stdin.readLineSync()!);
     }
   } catch (e) {
     print("Erro: ${e}. Tente novamente, mais tarde!");
   }
-    switch (escolheProduto) {
-      case 'Hamburguer de Siri':
-      print("Informe o valor que deseja adicionar:");
-      valor_desejada = double.parse(stdin.readLineSync()!);
-      if(valor_desejada == true){
+      valorCompra = double.parse(stdin.readLineSync()!);
+      if(valorCompra == true){
         print("Valor ${escolheProduto} adicionado ao Hamburguer de Siri");
       } else {
-        while(valor_desejada.isNaN){
+        try {
+        while(valorCompra.isNaN || ){
           print("Você não adicionou nenhum produto, tente novamente");
-          valor_desejada = double.parse(stdin.readLineSync()!);
+          valorCompra = double.parse(stdin.readLineSync()!);
         }
-        
+        } catch(e){
+            print("").
+        }
       }
         break;
       case 'Pão de Queijo':
-      print("Informe o valor que deseja adicionar:");
-      valor_desejada = double.parse(stdin.readLineSync()!);
+  try {
+    print("Qual valor você deseja informar?");
+    valorCompra = double.parse(stdin.readLineSync()!);
+    while (valorCompra.isNaN || valorCompra == 0 || valorCompra.isNegative) {
+      print("O valor da compra não pode ser nulo, negativo e igual a 0. Tente novamente!");
+      valorCompra = double.parse(stdin.readLineSync()!);
+    }
+    FormatException(e) {
+      print("Erro: Não pode ser convertido em String. Digite mais uma vez!");
+      valorCompra = double.parse(stdin.readLineSync()!);
+    }
+  } catch (e) {
+    print("Erro: ${e}. Tente novamente, mais tarde!");
+  }
+      valorCompra = double.parse(stdin.readLineSync()!);
+      if(valorCompra == true){
+        print("Valor ${escolheProduto} adicionado ao Hamburguer de Siri");
+      } else {
+        while(valorCompra.isNaN){
+          print("Você não adicionou nenhum produto, tente novamente");
+          valorCompra = double.parse(stdin.readLineSync()!);
+        }
+      }
 
         break;
       case 'BIS Chocolate':
-      print("Informe o valor que deseja adicionar:");
-      valor_desejada = double.parse(stdin.readLineSync()!);
-
+  try {
+    print("Qual valor você deseja informar?");
+    valorCompra = double.parse(stdin.readLineSync()!);
+    while (valorCompra.isNaN || valorCompra == 0 || valorCompra.isNegative) {
+      print("O valor da compra não pode ser nulo, negativo e igual a 0");
+      valorCompra = double.parse(stdin.readLineSync()!);
+    }
+    FormatException(e) {
+      print("Erro: Não pode ser convertido em String. Digite mais uma vez!");
+      valorCompra = double.parse(stdin.readLineSync()!);
+    }
+  } catch (e) {
+    print("Erro: ${e}. Tente novamente, mais tarde!");
+  }
+      valorCompra = double.parse(stdin.readLineSync()!);
+      if(valorCompra == true){
+        print("Valor ${escolheProduto} adicionado ao Hamburguer de Siri");
+      } else {
+        while(valorCompra.isNaN){
+          print("Você não adicionou nenhum produto, tente novamente");
+          valorCompra = double.parse(stdin.readLineSync()!);
+        }
+      }
         break;
       default:
         print("Opção inválida, tente novamente");
+        escolheProduto = stdin.readLineSync();
     }
+   } while(escolheProduto != 'BIS Chocolate' || escolheProduto != "Pão de Queijo" || escolheProduto != 'Hamburguer de Siri'); 
 }
