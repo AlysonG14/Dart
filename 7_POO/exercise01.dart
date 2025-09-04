797056cc245f972e9bf8c1486d0d7d5232588a4e
AlysonPereira <alyson.bosch.senai14@gmail.com>
2025-09-04 16:58:05 -0300

    Add files via upload

---
+++ 7_POO/exercise01.dart
@@ -0,0 +1,48 @@
+// Exercício 01 - Cachorro e Gato
+
+import 'dart:io';
+
+class Animal{
+  String nome;
+  int idade;
+
+  Animal({required this.nome, required this.idade});
+
+  void fazerSom(){
+    print('');
+  }
+    
+}
+
+class Cachorro extends Animal{
+  String? raca;
+
+  Cachorro({required String nome, required int idade, String? raca})
+  :super(nome: nome, idade: idade);
+
+  void fazerSom(){
+    print('Au Au');
+  }
+}
+
+class Gato extends Animal{
+  String? cor;
+
+  Gato({required String nome, required int idade, String? cor})
+  :super(nome: nome, idade: idade);
+
+  void fazerSom(){
+    print("Miau");
+  }
+}
+
+void main(){
+
+  Cachorro Spike = Cachorro(nome: 'Spike', idade: 4, raca: 'Bulldog');
+  Gato Tom = Gato(nome: 'Tom', idade: 2, cor: 'Azul');
+
+  Spike.fazerSom();
+  Tom.fazerSom();
+
+
+}
