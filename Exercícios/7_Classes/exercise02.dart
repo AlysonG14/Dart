a0355fc4c9474673da7314c375e4a1c34ba912a2
AlysonG14 <alyson.bosch.senai14@gmail.com>
2025-09-12 17:00:02 -0300

    Upload File 12/09

--- Exercícios/7_Classes/exercise02.dart
+++ Exercícios/7_Classes/exercise02.dart
@@ -2,72 +2,138 @@
 
 import 'dart:io';
 
-class Conta{
+class Conta {
   String nome_titular;
   double saldoInicial;
   double? investimento;
 
-  Conta({required this.nome_titular, required this.saldoInicial, this.investimento});
+  Conta({
+    required this.nome_titular,
+    required this.saldoInicial,
+    this.investimento,
+  });
 
-  void informacoesCandidato(){
+  void informacoesCandidato() {
     print('Esse vai ser o Nome Titular (obrigatório)');
     print('Esse vai ser o Saldo do Candidato (obrigatório):');
     print('Esse vai ser o Investimento do Candidato (opcional):');
   }
 }
 
-class ContaCorrente extends Conta{
+class ContaCorrente extends Conta {
   double? limite_cheque;
-  ContaCorrente({required String nome_titular, required double saldoInicial, double? this.limite_cheque})
-  :super(nome_titular: nome_titular, saldoInicial: saldoInicial);
+  ContaCorrente({
+    required String nome_titular,
+    required double saldoInicial,
+    double? this.limite_cheque,
+  }) : super(nome_titular: nome_titular, saldoInicial: saldoInicial);
 
   @override
-  void informacoesCandidato(){
-    print('Nome Titular ${nome_titular}');
-    print('Saldo Atual: ${saldoInicial}');
-    if(investimento == null || investimento!.isNaN || investimento!.isNegative){
+  void informacoesCandidato() {
+    print('---------------------------------------------------');
+    print('Nome Titular: ${nome_titular}');
+    print('Saldo Atual: ${saldoInicial.toStringAsFixed(2)}');
+    if (investimento == null ||
+        investimento!.isNaN ||
+        investimento!.isNegative) {
       print('Não existe investimento nesse candidato');
+      print('---------------------------------------------------');
     } else {
-      print('Investimento ${investimento}');
+      print('Investimento ${investimento!.toStringAsFixed(2)}');
+      print('---------------------------------------------------');
     }
   }
-  
 
-  void limite(limite_cheque){
-    print('Qual limite da conta, você deve colocar, para o ${nome_titular}?');
+  void limiteCheque() {
+    print('Deseja colocar limite de cheque para o ${nome_titular}?');
     limite_cheque = double.parse(stdin.readLineSync()!);
-    print('Limite: ${limite_cheque}');
-    print('----------------------');
-  }
-
-}
-
-class ContaPoupanca extends Conta{
-  double taxaRendimento;
-  ContaPoupanca({required String nome_titular, required double saldoInicial, required double this.taxaRendimento})
-  :super(nome_titular: nome_titular, saldoInicial: saldoInicial);
-
-  @override
-  void informacoesCandidato(){
-    print('Nome Titular ${nome_titular}');
-    print('Saldo Atual: ${saldoInicial}');
-    if(investimento == null || investimento!.isNaN || investimento!.isNegative){
-      print('Não existe investimento nesse candidato!');
+    if (limite_cheque == 'S'.toLowerCase() ||
+        limite_cheque == 's'.toUpperCase()) {
+      print('Você digitou S para sair');
     } else {
-      print('Investimento: ${investimento}');
+      print('Limite: ${limite_cheque}');
     }
   }
-
-  void atualizaSaldo(int taxa){
-
-  }
 }
 
-void main(){
-  
+class ContaPoupanca extends Conta {
+  double taxaRendimento;
+  ContaPoupanca({
+    required String nome_titular,
+    required double saldoInicial,
+    required double this.taxaRendimento,
+  }) : super(nome_titular: nome_titular, saldoInicial: saldoInicial);
+
+  void atualizaSaldo(int taxa) {}
 }
 
-dynamic percentual(double saldo, percentual){
+void main() {
+  // String entradaCheque;
+  // int opcaoCandidatos;
+  // double cheque;
+  // dynamic adicionandoCheque;
+
+  ContaCorrente candidato1 = new ContaCorrente(
+    nome_titular: 'Murilo',
+    saldoInicial: 2000.00,
+  );
+  ContaCorrente candidato2 = new ContaCorrente(
+    nome_titular: 'David',
+    saldoInicial: 1800.00,
+  );
+  ContaCorrente candidato3 = new ContaCorrente(
+    nome_titular: 'Rafael',
+    saldoInicial: 2200.00,
+  );
+
+  candidato1.informacoesCandidato();
+  candidato1.limiteCheque();
+  candidato2.informacoesCandidato();
+  candidato2.limiteCheque();
+  candidato3.informacoesCandidato();
+  candidato3.limiteCheque();
+
+  // while (true) {
+  //   print(
+  //     'Deseja colocar limite de cheque dos candidatos? \nEscolhe uma opção: \n1- sim \n2- não',
+  //   );
+  //   entradaCheque = stdin.readLineSync()!;
+  //   if (entradaCheque == '2') {
+  //     print('Escolheu a opção NÃO.');
+  //     break;
+  //   } else if (entradaCheque == '1') {
+  //     do {
+  //       print('Candidatos Presentes:');
+  //       print('1- ${candidato1.nome_titular}');
+  //       print('2- ${candidato2.nome_titular}');
+  //       print('3- ${candidato3.nome_titular}');
+  //       opcaoCandidatos = int.parse(stdin.readLineSync()!);
+  //     } while (opcaoCandidatos < 1 || opcaoCandidatos > 3);
+  //
+  //     switch (entradaCheque) {
+  //       case '1':
+  //         print(
+  //           'Qual limite de Cheque, você vai colocar no ${candidato1.nome_titular}?',
+  //         );
+  //         cheque = double.parse(stdin.readLineSync()!);
+  //         adicionandoCheque = candidato1.limite_cheque;
+  //         candidato1.informacoesCandidato();
+  //         print('Limite: ${candidato1.limite_cheque}');
+  //         break;
+  //       case '2':
+  //         candidato2.informacoesCandidato();
+  //         break;
+  //       case '3':
+  //         candidato3.informacoesCandidato();
+  //         break;
+  //       default:
+  //         print('Opção Inválido! Escolhe a opção correta! 1 à 3');
+  //     }
+  //   }
+  // }
+}
+
+dynamic percentual(double saldo, percentual) {
   double porcentagem = saldo * (percentual / 100);
   double calcularPorcentagem = porcentagem;
   return calcularPorcentagem;
