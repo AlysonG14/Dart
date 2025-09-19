a808526ad7c7bd4adf335e458d23906fd6c63079
Alyson Pereira <alyson.bosch.senai14@gmail.com>
2025-09-18 21:41:35 -0300

    Upload File 18/09 - Feat: Implement Switch Case with list on class POO from the second avaliation of Dart, finishing him with 50%

--- Exercícios/8_Avaliação_POO/main.dart
+++ Exercícios/8_Avaliação_POO/main.dart
@@ -4,12 +4,16 @@ import 'dart:io';
 void main() {
   String nomeAnimal;
   String corAnimal;
+  String? entradaAnimal;
   int idadeAnimal;
   int? entradaOpcao;
-  int? entradaAnimal;
   List cadastramentoLeao = [];
   List cadastramentoMacaco = [];
   List cadastramentoGato = [];
+
+  List <Map<String, dynamic>> cadastroAnimalCompleto = [
+    {'nome': null, 'idade': 0, 'cor': null}
+  ];
 
   print('Animais cadastrados:');
   Leao Mufasa = new Leao(
@@ -62,63 +66,121 @@ void main() {
     do {
       switch (entradaOpcao) {
         case 1:
-          print('Escolhe qual animal você deseja cadastrar? (Digite S para sair)');
-          if(entradaAnimal == 's' || entradaAnimal == 'S'){
+          print(
+            'Escolhe qual animal você deseja cadastrar? (Digite S para sair)',
+          );
+          if (entradaAnimal == 's' || entradaAnimal == 'S') {
             print('Você saiu');
             break;
           }
-          print('1- Leão');
+          print('1- Leao');
           print('2- Macaco');
           print('3- Gato');
-          entradaAnimal = int.parse(stdin.readLineSync()!);
+          entradaAnimal = stdin.readLineSync()!;
           switch (entradaAnimal) {
-            case 1:
+
+            case 'Leao':
               print('Digite o nome: ');
               nomeAnimal = stdin.readLineSync()!;
+              cadastramentoLeao.add(nomeAnimal);
+              cadastroAnimalCompleto[0]['nome'] += cadastramentoLeao;
               print('Digite a idade: ');
               idadeAnimal = int.parse(stdin.readLineSync()!);
+              cadastramentoLeao.add(idadeAnimal);
+              cadastroAnimalCompleto[0]['idade'] += cadastramentoLeao;
               print('Digite a cor: ');
               corAnimal = stdin.readLineSync()!;
+              cadastramentoLeao.add(corAnimal);
+              cadastroAnimalCompleto[0]['cor'] += cadastramentoLeao;
+              print('Cadastramento completo.');
+
+              for(dynamic cadastroCompleto = 0; cadastroCompleto < cadastroAnimalCompleto.length; cadastroCompleto++){
+                cadastroCompleto = [nomeAnimal] + [idadeAnimal.toString()] + [corAnimal]; 
+                print("${cadastroCompleto} Cadastrado.");
+              }
+
               break;
-            case 2:
+            case 'Macaco':
+              print('Digite o nome: ');
+              nomeAnimal = stdin.readLineSync()!;
+              cadastramentoMacaco.add(nomeAnimal);
+              cadastroAnimalCompleto[1]['nome'] += cadastramentoMacaco;
+              print('Digite a idade: ');
+              idadeAnimal = int.parse(stdin.readLineSync()!);
+              cadastramentoMacaco.add(idadeAnimal);
+              cadastroAnimalCompleto[1]['idade'] += cadastramentoMacaco;
+              print('Digite a cor: ');
+              corAnimal = stdin.readLineSync()!;
+              cadastramentoMacaco.add(corAnimal);
+              cadastroAnimalCompleto[1]['cor'] += cadastramentoMacaco;
+              print('Cadastramento completo.');
+
+              for(dynamic cadastroCompleto = 0; cadastroCompleto < cadastroAnimalCompleto.length; cadastroCompleto++){
+                cadastroCompleto = [nomeAnimal] + [idadeAnimal.toString()] + [corAnimal]; 
+                print("${cadastroCompleto} Cadastrado.");
+              }
+
+              break;
+
+            case 'Gato':
               print('Digite o nome');
               nomeAnimal = stdin.readLineSync()!;
+              cadastramentoGato.add(nomeAnimal);
+              cadastroAnimalCompleto[0]['nome'] += cadastramentoGato;
               print('Digite a idade');
               idadeAnimal = int.parse(stdin.readLineSync()!);
+              cadastramentoGato.add(idadeAnimal);
+              cadastroAnimalCompleto[0]['idade'] += cadastramentoGato;
               print('Digite a cor');
               corAnimal = stdin.readLineSync()!;
-              break;
-            case 3:
-              print('Digite o nome');
-              nomeAnimal = stdin.readLineSync()!;
-              print('Digite a idade');
-              idadeAnimal = int.parse(stdin.readLineSync()!);
-              print('Digite a cor');
-              corAnimal = stdin.readLineSync()!;
+              cadastramentoGato.add(corAnimal);
+              cadastroAnimalCompleto[0]['cor'] += cadastramentoGato;
+              print('Cadastramento completo.');
+
+              for(dynamic cadastroCompleto = 0; cadastroCompleto < cadastroAnimalCompleto.length; cadastroCompleto++){
+                cadastroCompleto = [nomeAnimal] + [idadeAnimal.toString()] + [corAnimal]; 
+                print("${cadastroCompleto} Cadastrado.");
+              }
+
               break;
             default:
               print('Opção Inválido! Escolhe corretamente as opções 1, 2 e 3');
           }
         case 2:
           print('Escolhe qual animal você deseja listar?');
-          print('1- Leão');
-          print('2- Macaco');
-          print('3- Gato');
-          entradaAnimal = int.parse(stdin.readLineSync()!);
+          print('Leão');
+          print('Macaco');
+          print('Gato');
+          entradaAnimal = stdin.readLineSync()!;
+          switch(entradaAnimal){
+            case 'Leao':
+
+            for(dynamic cadastro in cadastroAnimalCompleto){
+              cadastro = cadastramentoLeao;
+            }
+
+            break;
+
+            case 'Macaco':
+            break;
+
+            case 'Gato':
+            break;
+          }
           break;
         case 3:
           print('Escolhe qual animal você deseja editar?');
-          print('1- Leão');
-          print('2- Macaco');
-          print('3- Gato');
-          entradaAnimal = int.parse(stdin.readLineSync()!);
+          print('Leão');
+          print('Macaco');
+          print('Gato');
+          entradaAnimal = stdin.readLineSync()!;
           break;
         case 4:
           print('Escolhe qual animal você deseja remover?');
-          print('1- Leão');
-          print('2- Macaco');
-          print('3- Gato');
-          entradaAnimal = int.parse(stdin.readLineSync()!);
+          print('Leão');
+          print('Macaco');
+          print('Gato');
+          entradaAnimal = stdin.readLineSync()!;
           break;
         default:
           print('Opção Inválido, escolhe corretamente as opções: 1, 2, 3 e  4');
